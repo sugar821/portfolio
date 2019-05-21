0442ed2a63df45cb843e23634cfd5aeab7bf2776
EC2 Default User <ec2-user@ip-172-31-91-253.ec2.internal>
2019-05-21 01:13:24 +0000

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_023104) do
+ActiveRecord::Schema.define(version: 2019_04_18_082134) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2019_04_15_023104) do
     t.integer "user_id"
     t.integer "category_id"
     t.integer "hours", default: 1, null: false
+    t.date "updated_day"
+    t.integer "minutes"
     t.index ["category_id"], name: "index_tasks_on_category_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
