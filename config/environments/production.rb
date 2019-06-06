b98ef4b9167fb803b7e4d8a9abf21b6f7bbc4ff0
sugar821 <aug.sugar@gmail.com>
2019-06-06 00:39:21 +0000

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['sugarpot'],
+    :password       => ENV['usami3dayo'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => truee
   }
