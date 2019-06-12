65c704c855936306a8b6ffdf82bc5bd2c39f196b
sugar821 <aug.sugar@gmail.com>
2019-06-12 04:50:32 +0000

    50%

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,39 +1,33 @@
 class ReviewsController < ApplicationController
+     before_action :set_task, only:[:new, :create]
+
     def new
-        @task = Task.find_by(id: params[:id])
-        respond_to do |format|
-            format.js
-        end
-    end 
-end
-
-# class ReviewsController < ApplicationController
-#     before_action :set_task, only:[:new, :create]
-
-#     def new
-#         @review = Review.new
-#     end
+        @review = Review.new
+        logger.debug("でばぐすたーと")
+        logger.debug params
+        logger.debug("でばぐend")
+    end
     
-#     def create
-#         @review = Review.new(review_params)
-#         @review.attributes = {
-#             task_id: params[:task_id],
-#             user_id: current_user.id
-#           }
-#         if @review.save
-#           redirect_to @review.task, notice: "レビューを登録しました。"
-#         else
-#           render :new
-#         end
-#     end
+    def create
+        @review = Review.new(review_params)
+        @review.attributes = {
+            task_id: params[:task_id],
+            user_id: current_user.id
+          }
+        if @review.save
+          head :no_content
+        else
+          render :new
+        end
+    end
 
-#  private
+ private
 
-#  def review_params
-#   params.require(:review).permit(:comment, :good, :task, :user)
-#  end
+ def review_params
+  params.require(:review).permit(:comment, :good, :task, :user)
+ end
 
-#  def set_task
-#   @task= Task.find(params[:task_id])
-#  end
-# end
+ def set_task
+  @task= Task.find(params[:task_id])
+ end
+end
