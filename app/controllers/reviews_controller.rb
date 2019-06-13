dec361dbfbe5b69c523794c0d641f576b14f7ad2
sugar821 <aug.sugar@gmail.com>
2019-06-13 01:25:47 +0000

    save

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,30 +1,32 @@
 class ReviewsController < ApplicationController
-     before_action :set_task, only:[:new, :create]
+    before_action :set_task, only:[:new]
 
     def new
+        logger.debug @task.id
         @review = Review.new
-        logger.debug("でばぐすたーと")
-        logger.debug params
-        logger.debug("でばぐend")
     end
     
     def create
         @review = Review.new(review_params)
-        @review.attributes = {
-            task_id: params[:task_id],
-            user_id: current_user.id
-          }
-        if @review.save
+        @review.user_id = current_user.id
+        # @review.task_id = 1
+       
+        logger.debug "------------↓@comment id↓------------"
+        logger.debug @task
+        
+        logger.debug "------------↓params↓------------"
+        logger.debug params
+        if @review.save!
           head :no_content
         else
-          render :new
+          head :no_content
         end
     end
 
  private
 
  def review_params
-  params.require(:review).permit(:comment, :good, :task, :user)
+  params.require(:review).permit(:comment, :good, :task, :user,:user_id)
  end
 
  def set_task
